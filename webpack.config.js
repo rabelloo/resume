65ecef3a370dc8d0cf6ee3cae702d83f54e34467
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2018-08-04 00:46:47 -0300

    chore: env config

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader',
+        ]
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true,
+    }),
+  ],
+  serve: {
+    content: path.resolve(__dirname, 'dist'),
+    port: 4200,
+    clipboard: false,
+    hmr: true,
+    // http2: true,   // needs an SSL key configured
+    open: true,
+  },
+};
