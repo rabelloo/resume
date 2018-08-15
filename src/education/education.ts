6a51fa326254024bda9e1d807aadaa715579e524
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2018-08-15 19:13:57 -0300

    feat(education): initial implementation

---
+++ src/education/education.ts
@@ -0,0 +1,8 @@
+import { Component, WebComponent } from '../web-component';
+
+@Component({
+  selector: 'alr-education',
+  styles: '~education',
+  template: '~education',
+})
+export class Education extends WebComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './card/card';
 import './chip/chip';
+import './education/education';
 import './languages/languages';
 import './resume/resume';
 import './skills/skills';
