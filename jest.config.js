82583ab454980830326634b19a9ee8fceb1d8823
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2019-09-26 20:57:23 +0100

    chore: update packages

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfigFile: './tsconfig.test.json',
+      tsConfig: './tsconfig.test.json',
     },
   },
   moduleFileExtensions: ['ts', 'js'],
