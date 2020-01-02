1dcea1eedb7a1dbe0707f12eca89a76749e08d1a
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2020-01-02 20:07:50 +0000

    chore: update packages

--- scripts/web-component.js
+++ scripts/web-component.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-const sass = require('node-sass');
+const sass = require('sass');
 
 const srcFolder = 'src';
 
 module.exports = source =>
-  source
-  .replace(
-      /(template|styles)\s*:\s*(['"`])(?:~(.+?)|(.+?\.(html|css|s[ac]ss)))\2/g,
-      (_, prop, __, component, file, extension) => {
-        if (component) {
-          [file, extension] = findFile(prop, component);
-        }
-
-        return `${prop}: \`${ getContent(file, extension) }\``;
+  source.replace(
+    /(template|styles)\s*:\s*(['"`])(?:~(.+?)|(.+?\.(html|css|s[ac]ss)))\2/g,
+    (_, prop, __, component, file, extension) => {
+      if (component) {
+        [file, extension] = findFile(prop, component);
       }
-    );
+
+      return `${prop}: \`${getContent(file, extension)}\``;
+    }
+  );
 
 //////////////
 
@@ -27,7 +26,7 @@ const findFile = (prop, component) => {
     throw new Error(`Provided file not found: ${path}.css|scss|sass`);
   }
 
-  return [ `${path}.${extension}`, extension ];
+  return [`${path}.${extension}`, extension];
 };
 
 const getContent = (file, extension) => {
@@ -35,14 +34,13 @@ const getContent = (file, extension) => {
     return wrapStyles(
       sass
         .renderSync({ file: path.resolve(file) })
-        .css
-        .toString()
+        .css.toString()
         .replace(/:host([^() ,{]+)/g, ':host($1)')
     );
   }
 
   if (extension === 'css') {
-    return wrapStyles( read(file) );
+    return wrapStyles(read(file));
   }
 
   return read(file);
@@ -57,9 +55,11 @@ const ref = path => {
 
 const fileExists = file => fs.existsSync(path.resolve(file));
 const read = file => fs.readFileSync(path.resolve(file)).toString();
-const styleIs = (path, extension) => fileExists(`${path}.${extension}`) && extension;
+const styleIs = (path, extension) =>
+  fileExists(`${path}.${extension}`) && extension;
+// prettier-ignore
 const whatStyle = path => styleIs(path, 'css')
                        || styleIs(path, 'scss')
                        || styleIs(path, 'sass');
 
-const wrapStyles = styles => `<style>\n${ styles }</style>`;
+const wrapStyles = styles => `<style>\n${styles}</style>`;
