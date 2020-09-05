ca8fb15f6ced3161f6d5bfa70154ac77591700aa
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2020-09-05 15:18:47 +0100

    chore: clean up build script and travis.yml

--- scripts/web-component.js
+++ scripts/web-component.js
@@ -4,7 +4,7 @@ const sass = require('sass');
 
 const srcFolder = 'src';
 
-module.exports = source =>
+module.exports = (source) =>
   source.replace(
     /(template|styles)\s*:\s*(['"`])(?:~(.+?)|(.+?\.(html|css|s[ac]ss)))\2/g,
     (_, prop, __, component, file, extension) => {
@@ -22,9 +22,8 @@ const findFile = (prop, component) => {
   const path = ref(component);
   const extension = prop === 'template' ? 'html' : whatStyle(path);
 
-  if (!extension) {
+  if (!extension)
     throw new Error(`Provided file not found: ${path}.css|scss|sass`);
-  }
 
   return [`${path}.${extension}`, extension];
 };
@@ -46,20 +45,22 @@ const getContent = (file, extension) => {
   return read(file);
 };
 
-const ref = path => {
+const ref = (path) => {
   const folders = path.split('/');
   const name = folders.pop();
 
   return `${srcFolder}/${folders.join('/')}/${name}/${name}`;
 };
 
-const fileExists = file => fs.existsSync(path.resolve(file));
-const read = file => fs.readFileSync(path.resolve(file)).toString();
+const fileExists = (file) => fs.existsSync(path.resolve(file));
+const read = (file) => fs.readFileSync(path.resolve(file)).toString();
+
 const styleIs = (path, extension) =>
   fileExists(`${path}.${extension}`) && extension;
+
 // prettier-ignore
 const whatStyle = path => styleIs(path, 'css')
                        || styleIs(path, 'scss')
                        || styleIs(path, 'sass');
 
-const wrapStyles = styles => `<style>\n${styles}</style>`;
+const wrapStyles = (styles) => `<style>\n${styles}</style>`;
